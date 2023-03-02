d427eb7d1889d7d4a9b5de98a7a6aecf3d950875
Nick Hordijk <yourhomieniki@gmail.com>
2023-03-02 18:04:49 +0100

    Changed controller script, added minigame button.

--- Context 1/Assets/Scripts/PlayerTwoController.cs
+++ Context 1/Assets/Scripts/PlayerTwoController.cs
@@ -1,45 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(CharacterController))]
-public class PlayerController : MonoBehaviour
+public class PlayerTwoController : MonoBehaviour
 {
-    private CharacterController controller2;
-    private Vector3 playerVelocity2;
-    private bool groundedPlayer2;
     private float playerSpeed2 = 4.0f;
-    private float jumpHeight2 = 1.0f;
-    private float gravityValue2 = -9.81f;
-
-    private void Awake()
-    {
-        controller2 = GetComponent<CharacterController>();
-    }
-
     void Update()
     {
-        groundedPlayer2 = controller2.isGrounded;
-        if (groundedPlayer2 && playerVelocity2.y < 0)
-        {
-            playerVelocity2.y = 0f;
-        }
-
         Vector3 move = new Vector3(Input.GetAxis("Horizontal2"), 0, Input.GetAxis("Vertical2"));
-        controller2.Move(move * Time.deltaTime * playerSpeed2);
-
+   
         if (move != Vector3.zero)
         {
+            Vector3 movePos = transform.position + move * Time.deltaTime * playerSpeed2;
+            transform.position = movePos;
             gameObject.transform.forward = move;
         }
-
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer2)
-        {
-            playerVelocity2.y += Mathf.Sqrt(jumpHeight2 * -3.0f * gravityValue2);
-        }
-
-        playerVelocity2.y += gravityValue2 * Time.deltaTime;
-        controller2.Move(playerVelocity2 * Time.deltaTime);
     }
 }
--- Context 1/Assets/Scripts/TriggerEvent.cs
+++ Context 1/Assets/Scripts/TriggerEvent.cs
@@ -6,14 +6,47 @@ using UnityEngine.Events;
 public class TriggerEvent : MonoBehaviour
 {
     public UnityEvent onEnter, onExit;
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player1Tp;
+    public GameObject player2Tp;
+    private bool player1IsIn;
+    private bool player2IsIn;
 
     private void OnTriggerEnter(Collider other)
     {
         onEnter.Invoke();
+        if (other.gameObject == player1)
+        {
+            player1IsIn = true;
+        }
+        
+        if (other.gameObject == player2)
+        {
+            player2IsIn = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         onExit.Invoke();
+        if (other.gameObject == player1)
+        {
+            player1IsIn = false;
+        }
+
+        if (other.gameObject == player2)
+        {
+            player2IsIn = false;
+        }
+    }
+    private void Update()
+    {
+        if (player1IsIn && player2IsIn)
+        {
+            player1.transform.position = player1Tp.transform.position;
+            player2.transform.position = player2Tp.transform.position;
+            Debug.Log("AAAAAAAAAAAAAAA");
+        }
     }
 }
